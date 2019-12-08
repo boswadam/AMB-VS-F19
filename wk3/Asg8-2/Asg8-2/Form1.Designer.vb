628cfd723bab4b13e583cd7beeda22d709929fca
Adam Boswell <54445045+boswadam@users.noreply.github.com>
2019-12-08 13:47:27 -0700

    Asg 8-2 push

---
+++ wk3/Asg8-2/Asg8-2/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Dice_Tracker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.rollDiceButton = New System.Windows.Forms.Button()
+        Me.listBoxViewer = New System.Windows.Forms.ListBox()
+        Me.rollCountMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'rollDiceButton
+        '
+        Me.rollDiceButton.AllowDrop = True
+        Me.rollDiceButton.Font = New System.Drawing.Font("Microsoft YaHei", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rollDiceButton.Location = New System.Drawing.Point(119, 27)
+        Me.rollDiceButton.Name = "rollDiceButton"
+        Me.rollDiceButton.Size = New System.Drawing.Size(290, 70)
+        Me.rollDiceButton.TabIndex = 0
+        Me.rollDiceButton.Text = "Roll Dice"
+        Me.rollDiceButton.UseVisualStyleBackColor = True
+        '
+        'listBoxViewer
+        '
+        Me.listBoxViewer.FormattingEnabled = True
+        Me.listBoxViewer.Location = New System.Drawing.Point(119, 103)
+        Me.listBoxViewer.Name = "listBoxViewer"
+        Me.listBoxViewer.Size = New System.Drawing.Size(472, 290)
+        Me.listBoxViewer.TabIndex = 1
+        '
+        'rollCountMaskedTextBox
+        '
+        Me.rollCountMaskedTextBox.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip
+        Me.rollCountMaskedTextBox.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rollCountMaskedTextBox.Location = New System.Drawing.Point(451, 55)
+        Me.rollCountMaskedTextBox.Mask = "0000000"
+        Me.rollCountMaskedTextBox.Name = "rollCountMaskedTextBox"
+        Me.rollCountMaskedTextBox.Size = New System.Drawing.Size(140, 33)
+        Me.rollCountMaskedTextBox.TabIndex = 2
+        Me.rollCountMaskedTextBox.Text = "0010000"
+        Me.rollCountMaskedTextBox.ValidatingType = GetType(Integer)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(415, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "How many times?"
+        '
+        'clearButton
+        '
+        Me.clearButton.AutoEllipsis = True
+        Me.clearButton.Font = New System.Drawing.Font("Microsoft YaHei", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearButton.Location = New System.Drawing.Point(119, 399)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(226, 70)
+        Me.clearButton.TabIndex = 4
+        Me.clearButton.Text = "Clear"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.AutoEllipsis = True
+        Me.exitButton.Font = New System.Drawing.Font("Microsoft YaHei", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitButton.Location = New System.Drawing.Point(351, 399)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(240, 70)
+        Me.exitButton.TabIndex = 5
+        Me.exitButton.Text = "Exit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'Dice_Tracker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 488)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.rollCountMaskedTextBox)
+        Me.Controls.Add(Me.listBoxViewer)
+        Me.Controls.Add(Me.rollDiceButton)
+        Me.Name = "Dice_Tracker"
+        Me.Text = "Dice Tracker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents rollDiceButton As Button
+    Friend WithEvents listBoxViewer As ListBox
+    Friend WithEvents rollCountMaskedTextBox As MaskedTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents clearButton As Button
+    Friend WithEvents exitButton As Button
+End Class
---
+++ wk3/Asg8-2/Asg8-2/Form1.vb
@@ -0,0 +1,82 @@
+﻿'Adam Boswell
+'RCET 0265
+'Asg 8-2
+'Dice Tracker
+'
+
+Option Explicit On
+Option Strict On
+
+Public Class Dice_Tracker
+
+    Function rollDice() As Integer 'Returns a dice total to be compared
+        Dim dice1 As Integer
+        Dim dice2 As Integer
+        Dim diceTotal As Integer
+        dice1 = CInt(Rnd() * 5 + 1)
+        dice2 = CInt(Rnd() * 5 + 1)
+        diceTotal = dice1 + dice2
+        Return diceTotal
+    End Function
+
+    Private Sub rollDiceButton_Click(sender As Object, e As EventArgs) Handles rollDiceButton.Click
+        Dim rollCount As Integer
+        Dim diceTotal As Integer
+        Dim diceCase(10) As Integer
+        rollCount = CInt(rollCountMaskedTextBox.Text)
+
+        For i = 1 To rollCount      'rolls the dice for user selected number of times
+            diceTotal = rollDice()
+
+            Select Case diceTotal   'case compares dicetotal and increments to dicecase(0-10) array 
+                Case 2
+                    diceCase(0) += 1
+                Case 3
+                    diceCase(1) += 1
+                Case 4
+                    diceCase(2) += 1
+                Case 5
+                    diceCase(3) += 1
+                Case 6
+                    diceCase(4) += 1
+                Case 7
+                    diceCase(5) += 1
+                Case 8
+                    diceCase(6) += 1
+                Case 9
+                    diceCase(7) += 1
+                Case 10
+                    diceCase(8) += 1
+                Case 11
+                    diceCase(9) += 1
+                Case 12
+                    diceCase(10) += 1
+            End Select
+
+        Next
+
+        listBoxViewer.Items.Add("Two dice were rolled " & CStr(rollCountMaskedTextBox.Text) & " times total.")
+        listBoxViewer.Items.Add("The results of each roll were:" & vbNewLine)
+        listBoxViewer.Items.Add("The number 2 was rolled " & CStr(diceCase(0)) & " times")
+        listBoxViewer.Items.Add("The number 3 was rolled " & CStr(diceCase(1)) & " times")
+        listBoxViewer.Items.Add("The number 4 was rolled " & CStr(diceCase(2)) & " times")
+        listBoxViewer.Items.Add("The number 5 was rolled " & CStr(diceCase(3)) & " times")
+        listBoxViewer.Items.Add("The number 6 was rolled " & CStr(diceCase(4)) & " times")
+        listBoxViewer.Items.Add("The number 7 was rolled " & CStr(diceCase(5)) & " times")
+        listBoxViewer.Items.Add("The number 8 was rolled " & CStr(diceCase(6)) & " times")
+        listBoxViewer.Items.Add("The number 9 was rolled " & CStr(diceCase(7)) & " times")
+        listBoxViewer.Items.Add("The number 10 was rolled " & CStr(diceCase(8)) & " times")
+        listBoxViewer.Items.Add("The number 11 was rolled " & CStr(diceCase(9)) & " times")
+        listBoxViewer.Items.Add("The number 12 was rolled " & CStr(diceCase(10)) & " times")
+
+
+    End Sub
+
+    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        listBoxViewer.Items.Clear()
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Me.Close()
+    End Sub
+End Class
