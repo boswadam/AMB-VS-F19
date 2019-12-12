b0b18d537a3837793895666e8fa418c4aa50c3e4
Adam Boswell <54445045+boswadam@users.noreply.github.com>
2019-12-12 14:29:27 -0700

    Asg 7-1 push

---
+++ Asg 7-1/Asg 7-1/Form1.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Stock_Calculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MondayTextBox = New System.Windows.Forms.TextBox()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.StockList = New System.Windows.Forms.ListBox()
+        Me.TuesdayTextBox = New System.Windows.Forms.TextBox()
+        Me.WednesdayTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.StockSymbolTextBox = New System.Windows.Forms.TextBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SummaryButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CalculateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MondayTextBox
+        '
+        Me.MondayTextBox.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MondayTextBox.Location = New System.Drawing.Point(590, 75)
+        Me.MondayTextBox.Name = "MondayTextBox"
+        Me.MondayTextBox.Size = New System.Drawing.Size(136, 28)
+        Me.MondayTextBox.TabIndex = 0
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CalculateButton.Location = New System.Drawing.Point(437, 232)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(150, 62)
+        Me.CalculateButton.TabIndex = 3
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'StockList
+        '
+        Me.StockList.FormattingEnabled = True
+        Me.StockList.Location = New System.Drawing.Point(66, 124)
+        Me.StockList.Name = "StockList"
+        Me.StockList.Size = New System.Drawing.Size(350, 238)
+        Me.StockList.TabIndex = 4
+        '
+        'TuesdayTextBox
+        '
+        Me.TuesdayTextBox.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TuesdayTextBox.Location = New System.Drawing.Point(590, 124)
+        Me.TuesdayTextBox.Name = "TuesdayTextBox"
+        Me.TuesdayTextBox.Size = New System.Drawing.Size(136, 28)
+        Me.TuesdayTextBox.TabIndex = 5
+        '
+        'WednesdayTextBox
+        '
+        Me.WednesdayTextBox.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WednesdayTextBox.Location = New System.Drawing.Point(590, 180)
+        Me.WednesdayTextBox.Name = "WednesdayTextBox"
+        Me.WednesdayTextBox.Size = New System.Drawing.Size(136, 28)
+        Me.WednesdayTextBox.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(490, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 21)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Monday"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(490, 131)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 21)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Tuesday"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(490, 187)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 21)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Wednesday"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(585, 33)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(150, 29)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Stock Price"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(61, 68)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(166, 29)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Stock Option"
+        '
+        'StockSymbolTextBox
+        '
+        Me.StockSymbolTextBox.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StockSymbolTextBox.Location = New System.Drawing.Point(233, 71)
+        Me.StockSymbolTextBox.Name = "StockSymbolTextBox"
+        Me.StockSymbolTextBox.Size = New System.Drawing.Size(183, 28)
+        Me.StockSymbolTextBox.TabIndex = 11
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClearButton.Location = New System.Drawing.Point(616, 232)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(150, 62)
+        Me.ClearButton.TabIndex = 13
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SummaryButton
+        '
+        Me.SummaryButton.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SummaryButton.Location = New System.Drawing.Point(437, 300)
+        Me.SummaryButton.Name = "SummaryButton"
+        Me.SummaryButton.Size = New System.Drawing.Size(150, 62)
+        Me.SummaryButton.TabIndex = 14
+        Me.SummaryButton.Text = "Summary"
+        Me.SummaryButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(616, 300)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(150, 62)
+        Me.ExitButton.TabIndex = 15
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 16
+        Me.MenuStrip1.Text = "File"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CloseToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CalculateToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'CalculateToolStripMenuItem
+        '
+        Me.CalculateToolStripMenuItem.Name = "CalculateToolStripMenuItem"
+        Me.CalculateToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CalculateToolStripMenuItem.Text = "Calculate"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'Stock_Calculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SummaryButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.StockSymbolTextBox)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WednesdayTextBox)
+        Me.Controls.Add(Me.TuesdayTextBox)
+        Me.Controls.Add(Me.StockList)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.MondayTextBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Stock_Calculator"
+        Me.Text = "Stock Calculator "
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MondayTextBox As TextBox
+    Friend WithEvents CalculateButton As Button
+    Friend WithEvents StockList As ListBox
+    Friend WithEvents TuesdayTextBox As TextBox
+    Friend WithEvents WednesdayTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents StockSymbolTextBox As TextBox
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SummaryButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CalculateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Asg 7-1/Asg 7-1/Form1.vb
@@ -0,0 +1,131 @@
+﻿'Adam Boswell
+'RCET 0265
+'Asg 7-1
+'
+
+
+Option Strict On
+Option Explicit On
+
+Public Class Stock_Calculator
+    Dim summary As New List(Of String)
+    Dim stopSummary As New List(Of String)
+    Private Sub clearData()
+        StockSymbolTextBox.Clear()
+        MondayTextBox.Clear()
+        TuesdayTextBox.Clear()
+        WednesdayTextBox.Clear()
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        clearData()
+        StockList.Items.Clear()
+    End Sub
+
+    Private Function ColumnHeaders(firstColumn As String, secondColumn As String, thirdColumn As String, fourthColumn As String) As String
+
+        Dim columnWidth As Integer = 20
+        firstColumn = "|      " & firstColumn
+        secondColumn = "|      " & secondColumn
+        thirdColumn = "|      " & thirdColumn
+        fourthColumn = "|      " & fourthColumn
+
+        Return firstColumn.PadRight(columnWidth) & secondColumn.PadRight(columnWidth) &
+            thirdColumn.PadRight(columnWidth) & fourthColumn.PadRight(columnWidth)
+
+    End Function
+    Private Sub calculate()
+
+        Dim errorMessage As String = ""
+        Dim firstDayChange As Double
+        Dim secondDayChange As Double
+        Dim decision As String
+        Static buy As Integer = 0
+        Static sell As Integer = 0
+        Static hold As Integer = 0
+        'try catch statements are used to filter out bad data
+        Try
+            Double.Parse(WednesdayTextBox.Text)
+        Catch ex As Exception
+            errorMessage = "Please enter a valid Price in Wednesday." & vbNewLine & errorMessage
+            WednesdayTextBox.Select()
+        End Try
+        Try
+            Double.Parse(TuesdayTextBox.Text)
+        Catch ex As Exception
+            errorMessage = "Please enter a valid Price in Tuesday." & vbNewLine & errorMessage
+            TuesdayTextBox.Select()
+        End Try
+        Try
+            Double.Parse(MondayTextBox.Text)
+        Catch ex As Exception
+            errorMessage = "Please enter a valid Price in Monday." & vbNewLine & errorMessage
+            MondayTextBox.Select()
+        End Try
+        If StockSymbolTextBox.Text = "" Then
+            errorMessage = "Please enter the stock code you would like to calculate." & vbNewLine & errorMessage
+            StockSymbolTextBox.Select()
+        End If
+        'if the error box is not empty it will show the errors found above
+        If errorMessage <> "" Then
+            MessageBox.Show(errorMessage)
+        End If
+
+        If errorMessage = "" Then
+            firstDayChange = Double.Parse(TuesdayTextBox.Text) - Double.Parse(MondayTextBox.Text)
+            secondDayChange = Double.Parse(WednesdayTextBox.Text) - Double.Parse(TuesdayTextBox.Text)
+            decision = CStr(HoldBuySell(firstDayChange, secondDayChange))
+            'If error box is empty then no errors were found and the data is compared to produce a buy sell or hold result
+            Select Case decision
+                Case "Buy"
+                    summary.Insert(0, ColumnHeaders(StockSymbolTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    StockList.Items.Insert(0, ColumnHeaders(StockSymbolTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    buy += 1
+                Case "Sell"
+                    summary.Insert(buy, ColumnHeaders(StockSymbolTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    StockList.Items.Insert(buy, ColumnHeaders(StockSymbolTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    sell += 1
+                Case "Hold"
+                    stopSummary.Add(ColumnHeaders(StockSymbolTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+                    StockList.Items.Insert((buy + sell), ColumnHeaders(StockSymbolTextBox.Text, firstDayChange.ToString("C"), secondDayChange.ToString("C"), decision))
+            End Select
+
+            clearData()
+        End If
+    End Sub
+
+
+    Private Function HoldBuySell(tuesdayChange As Double, wednesdayChange As Double) As Object
+        Dim result As String
+        If tuesdayChange > 0 And wednesdayChange > tuesdayChange Then
+            result = "Buy"
+        ElseIf tuesdayChange < 0 And wednesdayChange < tuesdayChange Then
+            result = "Sell"
+        Else
+            result = "Hold"
+        End If
+
+        Return result
+    End Function
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CalculateToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles CalculateToolStripMenuItem.Click
+        calculate()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        clearData()
+        StockList.Items.Clear()
+    End Sub
+
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        calculate()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
